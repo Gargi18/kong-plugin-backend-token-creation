b6a1e8b5dcb9aa41fb2617c5056b8e784e3b6cfe
mod42 <steffen@kong.rocks>
2023-12-15 10:22:11 +0100

    first commit

---
+++ backend-oidc/handler.lua
@@ -0,0 +1,47 @@
+local kong = kong
+local cjson = require "cjson"
+local http = require "resty.http"
+
+local plugin = {
+  PRIORITY = 10,
+  VERSION = "0.1",
+}
+
+function plugin:access(plugin_conf)
+
+   local httpc = http.new()
+   local res, err = httpc:request_uri("https://sso.apim.eu:8443/auth/realms/steffen/protocol/openid-connect/token", {
+     method = "POST",
+     body = "grant_type=client_credentials&client_id=" .. plugin_conf.client_id .. "&client_secret=" .. plugin_conf.client_secret .. "&scope=" .. plugin_conf.scope,
+     headers = {
+       ["Accept"] = "application/json",
+       ["Content-Type"] = "application/x-www-form-urlencoded",
+     },
+     ssl_verify = false
+   })
+
+   local body = res.body
+   local json_okta_response = cjson.decode(body)
+
+   if res.status == 200 then
+    kong.service.request.add_header("Authorization", "Bearer " .. json_okta_response.access_token)
+   else
+    return kong.response.exit(401, "Unauthorized")
+   end
+
+--    kong.log.debug("keycloak body" .. body) 
+
+  
+
+end
+-- return our plugin object
+return plugin
+
+
+
+
+
+
+
+
+
---
+++ backend-oidc/schema.lua
@@ -0,0 +1,18 @@
+
+local typedefs      = require "kong.db.schema.typedefs"
+
+return {
+  name = "backend-oidc",
+  fields = {
+    { consumer = typedefs.no_consumer },
+    { protocols = typedefs.protocols_http },
+    { config = {
+        type = "record",
+        fields = {
+          { client_id = { type = "string", required = true }, },
+          { client_secret = { type = "string", required = false }, },
+          { scope = { type = "string", required = false }, },
+        },
+    }, },
+  },
+}
