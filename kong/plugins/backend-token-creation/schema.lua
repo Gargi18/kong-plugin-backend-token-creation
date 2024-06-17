ff159056c36435b3340a56f780f3de9ca5fa3393
Gargi18 <gargi.talukdar@gmail.com>
2024-06-17 12:56:19 +0530

    Update schema.lua

--- kong/plugins/backend-token-creation/schema.lua
+++ kong/plugins/backend-token-creation/schema.lua
@@ -12,11 +12,9 @@ return {
           { token_url = { type = "string", required = true }, },
           { client_id = { type = "string", required = true }, },
           { client_secret = { type = "string", required = false }, },
+          { audience = { type = "string", required = false }, },
           { scope = { type = "string", required = false }, },
-          { ssl_verify = { type = "boolean", default = true, }, },
-          { credentials_send_in = {type = "string", default = "auth header", required = true, one_of = { "auth header", "body" },}, },
-          { upstream_appid_header_name = { type = "string", required = false, default="myappid" }, },
-          { upstream_appid_header_value = { type = "string", required = false, default="appid" }, },
+          { ssl_verify = { type = "boolean", default = true, }, }
         }
     }, },
   },
